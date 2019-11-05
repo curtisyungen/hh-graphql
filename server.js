4dd55f92cf30d74beb6728ea0dfda26d9ff09428
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-11-05 07:20:53 -0800

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ const app = express();
 //   credentials: true
 // };
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(cors());
 app.use('/graphql', graphqlHTTP({
     schema, 
