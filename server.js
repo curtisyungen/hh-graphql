5b3368ab18fa8eef407c8506c1a1480f7f132a4c
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-11-05 12:38:29 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('./src/graphql/schema');
 const cors = require('cors');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -11,12 +11,6 @@ const app = express();
 //   origin: 'https://hh-graphql.herokuapp.com/',
 //   credentials: true
 // };
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.use(cors());
 app.use('/graphql', graphqlHTTP({
