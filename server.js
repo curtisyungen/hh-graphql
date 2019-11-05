d8fb3c5de8a7b50f98b9c05b5816482008ad3c06
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-11-04 18:52:20 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./src/graphql/schema');
+const cors = require('cors');
+
+const PORT = 5000;
+
+const app = express();
+
+app.use(cors());
+app.use('/graphql', bodyParser.json(), graphqlHTTP({
+    schema, 
+    graphiql: true,
+}));
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`);
+});
+
+module.exports.app = app;
