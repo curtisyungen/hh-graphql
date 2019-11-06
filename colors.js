e733d65519c25afc88f7a2b1feb42b3d7351fc6f
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-11-06 08:24:26 -0800

    update lightenColor

--- colors.js
+++ colors.js
@@ -1,24 +1,28 @@
-function lightenColor(col, amt) {
-    var usePound = false;
-    if (col[0] == "#") {
-        col = col.slice(1);
-        usePound = true;
-    }
- 
-    var num = parseInt(col,16);
-    var r = (num >> 16) + amt;
+function lightenColor(hexCode, percentage) {
+
+    // Remove # symbol
+    hexCode = hexCode.slice(1);
+
+    // Convert to bit integer
+    let num = parseInt(hexCode, 16);
+    
+    // Adjust red
+    let r = (num >> 16) + percentage;
     if (r > 255) r = 255;
     else if  (r < 0) r = 0;
  
-    var b = ((num >> 8) & 0x00FF) + amt;
+    // Adjust blue
+    let b = ((num >> 8) & 0x00FF) + percentage;
     if (b > 255) b = 255;
     else if  (b < 0) b = 0;
  
-    var g = (num & 0x0000FF) + amt;
+    // Adjust green
+    let g = (num & 0x0000FF) + percentage;
     if (g > 255) g = 255;
     else if (g < 0) g = 0;
  
-    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+    // Return new hex code
+    return "#" + (g | (b << 8) | (r << 16)).toString(16);
 }
 
 let baseHexes = ['#CB4335', '#D35400', '#F1C40F', '#196F3D', '#21618C', '#5B2C6F', '#212F3C', '#979A9A'];
