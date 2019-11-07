92f87f550799815cb6f25342bdb4f1a2015c708c
Curtis Yungen <curtisyungen@gmail.com>
2019-11-06 21:45:07 -0800

    update build

---
+++ public/build/precache-manifest.40e22ed93253a8748a14e5a63d73c7c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c20c69c8f5d34e42a9b7372cb91e59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db61ffe9a10f0347e76b",
+    "url": "/static/css/main.1dd98967.chunk.css"
+  },
+  {
+    "revision": "482d788363e1b8855314",
+    "url": "/static/js/2.151841cb.chunk.js"
+  },
+  {
+    "revision": "db61ffe9a10f0347e76b",
+    "url": "/static/js/main.f271935e.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
